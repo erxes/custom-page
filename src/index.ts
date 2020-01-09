9189dba510d4d245aa5d22ad48268e50355cfa28
munkhsaikhan <munkhsaikhan.g@nmma.co>
2020-01-09 11:56:44 +0800

    erxes-api fetch

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import * as bodyParser from 'body-parser';
 import * as dotenv from 'dotenv';
 import * as express from 'express';
 import { connect } from './connection';
-import { checkTrigger } from './data/utils';
+import './messageBroker';
 
 // load environment variables
 dotenv.config();
@@ -24,20 +24,6 @@ app.use((req: any, _res, next) => {
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-app.post(`/check-trigger`, async (req: any, res) => {
-  // require login
-  console.log('****************************************************************************');
-  const postData = JSON.parse(req.body.postData);
-
-  if (!postData.userId) {
-    return res.end('foribidden');
-  }
-
-  const triggerResponse = await checkTrigger(postData);
-  const zz = res.end(JSON.stringify(triggerResponse));
-  return zz;
-});
 
 // Once the bot has booted up its internal services, you can use them to do stuff.
 
