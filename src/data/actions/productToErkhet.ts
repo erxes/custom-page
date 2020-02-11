7f3845dd73bc77a12e362d096b321a5dc238b883
munkhsaikhan <munkhsaikhan.g@nmma.co>
2020-02-12 02:57:16 +0800

    productCategory Ok

--- src/data/actions/productToErkhet.ts
+++ src/data/actions/productToErkhet.ts
@@ -1,30 +1,53 @@
-// import * as mongoose from 'mongoose';
+import * as mongoose from 'mongoose';
 import { sendMessage } from '../../messageBroker';
 import { IShapeDocument } from '../../models/definitions/Automations';
 
 const productToErkhet = async (shape: IShapeDocument, data: any, result: object) => {
-  const productData = data.doc;
-  const invData = {
-    action: data.action,
-    oldCode: data.oldCode || '',
-    object: {
-      code: productData.code || '',
-      name: productData.name || '',
-      measureUnit: productData.sku || '',
-      unitPrice: productData.unitPrice || 0,
-      costAccount: shape.config.costAccount,
-      saleAccount: shape.config.saleAccount,
-      categoryCode: productData.categoryCode,
-      defaultCategory: shape.config.categoryCode,
-    },
-  };
+  const objectData = data.doc;
+  let sendData = {};
+
+  if (data.action.includes('productCategory')) {
+    const { API_MONGO_URL = '' } = process.env;
+    const options = {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    };
+    const apiMongoClient = await mongoose.createConnection(API_MONGO_URL, options);
+    const apiProductCategories = apiMongoClient.db.collection('product_categories');
+    const productCategories = await apiProductCategories.find({ _id: objectData.parentId }).toArray();
+
+    sendData = {
+      action: data.action,
+      oldCode: data.oldCode || '',
+      object: {
+        code: objectData.code || '',
+        name: objectData.name || '',
+        parentCode: productCategories ? productCategories[0].code : '',
+      },
+    };
+  } else {
+    sendData = {
+      action: data.action,
+      oldCode: data.oldCode || '',
+      object: {
+        code: objectData.code || '',
+        name: objectData.name || '',
+        measureUnit: objectData.sku || '',
+        unitPrice: objectData.unitPrice || 0,
+        costAccount: shape.config.costAccount,
+        saleAccount: shape.config.saleAccount,
+        categoryCode: objectData.categoryCode,
+        defaultCategory: shape.config.categoryCode,
+      },
+    };
+  }
 
   const postData = {
     userEmail: shape.config.userEmail,
     token: shape.config.apiToken,
     apiKey: shape.config.apiKey,
     apiSecret: shape.config.apiSecret,
-    orderInfos: JSON.stringify(invData),
+    orderInfos: JSON.stringify(sendData),
   };
 
   sendMessage({
